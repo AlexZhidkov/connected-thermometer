ff9b20042778a9653bbecffc207549034d1189ef
Alexander Zhidkov <azhidkov@gmail.com>
2019-03-18 19:57:16 +0800

    add names to sensor data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireObject } from '@angular/fire/database';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,22 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   items: Observable<any[]>;
+  settingsRef: AngularFireObject<any>;
+  settings: any;
+
   constructor(db: AngularFireDatabase) {
-    this.items = db.list('data').valueChanges();
+    this.settingsRef = db.object('settings');
+    this.settingsRef.snapshotChanges().subscribe(action => {
+      this.settings = action.payload.val();
+
+      this.items = db.list('data').valueChanges().pipe(map(x => {
+        x.forEach((e) => {
+          const c = e as any;
+          c.name = this.settings.transmitter[c.transmitter_id].name;
+          c.sensors.forEach(s => s.name = this.settings.transmitter[c.transmitter_id].sensors[s.sensor - 1]);
+        });
+        return x;
+      }));
+    });
   }
 }
